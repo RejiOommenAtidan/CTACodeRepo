bb801a27f99272fac4a6d9cfb68aad544c2fb660
Malay Doshi <malay.doshi@atidan.com>
2020-08-25 20:12:33 +0530

    Greenbook Payment API Calls Base Class & Repo

---
+++ CTADBL/BaseClasses/GreenbookPayment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CTADBL.BaseClasses
+{
+    [Table("greenbookpayment")]
+    public class GreenbookPayment
+    {
+        #region GreenbookPayment Properties
+
+        [Key]
+        public int GreenbookID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string TibetianAssociation { get; set; }
+        public int YearOfLastPayment { get; set; }
+        public int NummberOfYears { get; set; }
+        public int EmployementYears { get; set; }
+        public int TotalDue { get; set; }
+        public int ExtraDonation { get; set; }
+
+        #endregion
+    }
+}
