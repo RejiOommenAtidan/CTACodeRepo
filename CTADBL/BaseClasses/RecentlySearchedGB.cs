f524fd0539af43acbca7d78a3a4c655022594ece
Malay Doshi <malay.doshi@atidan.com>
2020-09-07 18:12:40 +0530

    Recent Gb Stuff Added

---
+++ CTADBL/BaseClasses/RecentlySearchedGB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CTADBL.BaseClasses
+{
+    public class RecentlySearchedGB
+    {
+        #region Recently Searched GB
+        [Key]
+        public int ID { get; set; }
+        public int nGBID { get; set; }
+        public int nUserID { get; set; }
+        public string sEnteredDateTime { get; set; }
+        public int nEnteredBy { get; set; }
+        #endregion
+
+    }
+}
---
+++ CTAWebAPI/Controllers/RecentlySearchedGBController.cs
@@ -0,0 +1,45 @@
+﻿using CTADBL.BaseClasses;
+using CTADBL.BaseClassRepositories;
+using CTADBL.Entities;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CTAWebAPI.Controllers
+{
+    [EnableCors("AllowOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecentlySearchedGBController : ControllerBase
+    {
+        #region Constructor
+        private readonly DBConnectionInfo _info;
+        public RecentlySearchedGBController(DBConnectionInfo info)
+        {
+            _info = info;
+        }
+        #endregion
+
+        #region Get Recently Searched GB's
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetRecentlySearchedGBs()
+        {
+            #region Get Recently Searched GB's
+            try
+            {
+                RecentlySearchedGBRepository recentRepo = new RecentlySearchedGBRepository(_info.ConnectionString);
+                IEnumerable<RecentlySearchedGB> recentGBs = recentRepo.GetAllRecentlySearchedGB();
+                return Ok(recentGBs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
