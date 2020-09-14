5aecca9bdd370065595514aa6e6009526ab31cda
Rajen Parekh <Rajen.Parekh@atidan.com>
2020-09-14 17:35:28 +0530

    Bifurcated Dialog component from Country.js

---
+++ GreenbookCentralSystem/GreenbookCentralSysem/src/views/master/country/country.js
@@ -0,0 +1,422 @@
+// hi
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Container,
+  Grid,
+  Button,
+  Typography,
+  FormControl,
+  TextField
+} from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import theme from '../../../theme/theme/theme'
+import Page from 'src/components/Page';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import MUIDataTable from "mui-datatables";
+import { ThemeProvider } from '@material-ui/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Chip from '@material-ui/core/Chip';
+
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+
+// Local import
+import { AddDialog, DeleteDialog, EditDialog } from './dialog';
+
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    height: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+    flexGrow: 1,
+    'label + &': {
+      marginTop: theme.spacing(3)
+    }
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(0.5),
+    width: '100%'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  },
+  box: {
+    marginBottom: theme.spacing(1.5),
+    marginTop: theme.spacing(1.5)
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: red[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  }
+
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [editModal, setEditModal] = React.useState(false);
+  const [dataAPI, setdataAPI] = useState([]);
+  // const [loadingProp, setloadingProp] = useState(true);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [addModal, setAddModal] = useState(false);
+
+
+  //VAR
+  const [countryID, setCountryID] = React.useState('');
+  const [countryName, setCountryName] = React.useState('');
+  const [countryPK, setCountryPK] = React.useState(0);
+  const [countryObj, setCountryObj] = useState({});
+
+  const handleEditClickOpen = () => {
+    setEditModal(true);
+  };
+  const handleEditClickClose = () => {
+    setEditModal(false);
+  };
+  const handleAddClickOpen = () => {
+    setAddModal(true);
+  };
+  const handleAddClickClose = () => {
+    setAddModal(false);
+  };
+
+  const options = {
+    textLabels: {
+      body: {
+        noMatch: "Loading..."
+      }
+    },
+    filterType: 'checkbox',
+    selectableRows: false,
+    jumpToPage: true,
+    rowsPerPage: 5,
+    rowsPerPageOptions: [5, 10, 20, 30]
+  };
+
+  const columns = [
+    {
+      name: "id",
+      label: "Sr No.",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "sCountryID",
+      label: "Country ID",
+      options: {
+        filter: true,
+        sort: true
+      }
+    },
+    {
+      name: "sCountry",
+      label: "Country",
+      options: {
+        filter: true,
+        sort: true
+      }
+    },
+    {
+      name: "edit",
+      label: "Edit",
+      options: {
+        filter: false,
+        sort: false,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <Button
+              size="small"
+              variant="outlined"
+              color="primary"
+              startIcon={<EditOutlinedIcon />}
+              onClick={() => { editClick(tableMeta.rowData) }}
+            >Edit
+            </Button>
+          )
+        }
+      }
+    },
+    {
+      name: "delete",
+      label: "Delete",
+      options: {
+        filter: false,
+        sort: false,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              endIcon={<DeleteOutlinedIcon />}
+              onClick={() => { deleteClick(tableMeta.rowData) }}
+            >Delete
+            </Button>
+          )
+        }
+      }
+    }
+  ];
+
+  const editClick = (tableRowArray) => {
+    setCountryPK(tableRowArray[0]);
+    setCountryID(tableRowArray[1]);
+    setCountryName(tableRowArray[2]);
+    setEditModal(true);
+    setCountryObj({
+      id: tableRowArray[0],
+      countryId: tableRowArray[1],
+      countryName: tableRowArray[2]
+    });
+  }
+
+  const editAPICall = (countryObj) => {
+    // let CountryID = countryPK;
+    // let countryToUpdate = {
+    //   ID : countryPK,
+    //   sCountryID: countryID,
+    //   sCountry: countryName,
+    // };
+    axios.post(`/Country/EditCountry/CountryID=` + countryPK, countryObj/*countryToUpdate*/)
+      .then(resp => {
+        if (resp.status === 200) {
+          //console.log(resp.data);
+          setEditModal(false);
+          window.location = window.location;
+          // setdataAPI(dataAPI.map((data) => {
+          //   console.log(data);
+          //   if(data.id === countryObj.id){
+          //     console.log(data);
+          //     return {
+          //       ...data,
+          //       ...countryObj
+          //     };
+          //   }
+          //   else{
+          //     console.log(data)
+          //     return data;
+          //   }
+          // }))
+        }
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          console.warn(error.request);
+        } else {
+          console.error('Error', error.message);
+        }
+        console.log(error.config);
+      })
+      .then(release => {
+        //console.log(release); => udefined
+      });
+  };
+  const addAPICall = (countryObj) => {
+
+    // let countryToAdd = {
+    //   sCountryID: countryID,
+    //   sCountry: countryName,
+    // };
+    axios.post(`/Country/AddCountry/`, countryObj)
+      .then(resp => {
+        if (resp.status === 200) {
+          console.log(resp.data);
+          setAddModal(false);
+          window.location = window.location;
+        }
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          console.warn(error.request);
+        } else {
+          console.error('Error', error.message);
+        }
+        console.log(error.config);
+      })
+      .then(release => {
+        //console.log(release); => udefined
+      });
+  };
+
+  const deleteClick = (tableRowArray) => {
+
+    setDeleteModal(true);
+    setCountryPK(tableRowArray[0]);
+    setCountryID(tableRowArray[1]);
+    setCountryName(tableRowArray[2]);
+  };
+
+  const handleClose = () => {
+    setDeleteModal(false);
+
+  };
+
+  const deleteAPICall = () => {
+    // console.log(this.state.selectedUser);
+    // let CountryID = countryPK;
+    const countryToDelete = {
+      ID: countryPK,
+      sCountryID: countryID,
+      sCountry: countryName,
+    };
+    axios.post(`/Country/DeleteCountry/`, countryToDelete)
+      .then(resp => {
+        console.log(countryToDelete);
+        if (resp.status === 200) {
+          console.log(resp.data);
+          setDeleteModal(false);
+          window.location = window.location;
+          // setdataAPI(dataAPI.filter((data) => {
+          //   return (data.id !== countryToDelete.ID);
+          // }));
+        }
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          console.warn(error.request);
+        } else {
+          console.error('Error', error.message);
+        }
+        console.log(error.config);
+      })
+      .then(release => {
+        //console.log(release); => udefined
+      });
+  };
+
+  useEffect(() => {
+    axios.get(`/Country/GetCountries`)
+      .then(resp => {
+        if (resp.status === 200) {
+          console.log(resp.data);
+          setdataAPI(resp.data)
+        }
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error(error.response.data);
+          console.error(error.response.status);
+          console.error(error.response.headers);
+        } else if (error.request) {
+          console.warn(error.request);
+        } else {
+          console.error('Error', error.message);
+        }
+        console.log(error.config);
+      })
+      .then(release => {
+        //console.log(release); => udefined
+      });
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Page
+        className={classes.root}
+        title="Country"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          height="100%"
+          justifyContent="center"
+        >
+          <Container maxWidth="lg" disableGutters={true}>
+            <Typography variant="h4" gutterBottom>Country
+             <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                size="large"
+                //onClick={addClick()}
+                onClick={() => { setAddModal(true) }}
+              >
+                <AddCircleIcon />
+              </IconButton>
+            </Typography>
+            <Grid container className={classes.box}>
+              <Grid item xs={12}>
+                <MUIDataTable
+                  data={dataAPI}
+                  columns={columns}
+                  options={options}
+                />
+              </Grid>
+            </Grid>
+            {addModal && <AddDialog
+              addModal={addModal}
+              classes={classes}
+              handleAddClickClose={handleAddClickClose}
+              addAPICall={addAPICall}
+            />}
+            {editModal && <EditDialog
+              editModal={editModal}
+              countryObj={countryObj}
+              classes={classes}
+              handleEditClickClose={handleEditClickClose}
+              editAPICall={editAPICall}
+            />}
+            {deleteModal && <DeleteDialog
+              deleteModal={deleteModal}
+              countryName={countryName}
+              handleClose={handleClose}
+              deleteAPICall={deleteAPICall}
+            />}
+          </Container>
+        </Box>
+      </Page>
+
+
+    </ThemeProvider>
+  );
+}
