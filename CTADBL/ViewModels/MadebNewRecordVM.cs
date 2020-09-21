c216d427de05eaa83dd692392b44f07a68d9cffa
Rajen Parekh <Rajen.Parekh@atidan.com>
2020-09-21 15:02:15 +0530

    Created Base classes for New Madeb Empty Entry screen

---
+++ CTADBL/ViewModels/MadebNewRecordVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CTADBL.BaseClasses;
+
+namespace CTADBL.ViewModels
+{
+    public class MadebNewRecordVM
+    {
+        /*
+         * select Id, sMadebDisplayName from lstmadebtype;
+         * select ID, sAuthRegion from lstauthregion;
+         * select Id, sTypeIssued from lsttypeissued;
+	     * select 7000 as nFormNumber;
+	     * 
+         */
+        public List<AuthRegion> authRegions { get; set; }
+        public List<MadebType> madebTypes { get; set; }
+
+        public List<TypeIssued> typeIssued { get; set; }
+        public int nFormNumber { get; set; }
+
+    }
+}
