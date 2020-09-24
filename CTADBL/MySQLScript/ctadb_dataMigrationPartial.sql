c8b2deb416db12aad4d4cfbedc6b9b960e62ca5f
Reji Oommen <Reji.Oommen@razor-tech.com>
2020-09-24 18:05:08 +0530

    DBScript Given GB Id and Issued Book Data

---
+++ CTADBL/MySQLScript/ctadb_dataMigrationPartial.sql
@@ -0,0 +1,40 @@
+﻿use ctadb;
+
+INSERT INTO `ctadb`.`tblgivengbid`
+SELECT 
+	`gbnogiven`.`id`,
+	 `gbnogiven`.`id`,
+	`gbnogiven`.`IdentityID`,
+	`gbnogiven`.`formNo`,
+	`gbnogiven`.`date`,
+	`gbnogiven`.`GivenOrNot`,
+	IF(`gbnogiven`.`deleteTab`=1,0,1),
+	now(),
+	1,
+	now(),
+	1
+FROM `greenbookprime`.`gbnogiven`;
+
+INSERT INTO `ctadb`.`tblgreenbookissued`
+SELECT 
+	`ident_bookissued`.`BookIssuedID`,
+	`ident_bookissued`.`IdentityID`,
+	`ident_bookissued`.`IssuedDate`,
+	`ident_bookissued`.`WhyIssued`,
+	null,
+	`ident_bookissued`.`IssuedOrNot`,
+	`ident_bookissued`.`FormNo`,
+	`ident_bookissued`.`WhereIssued`,
+	`ident_bookissued`.`Printed`,
+	`ident_bookissued`.`Entered`,
+	`ident_bookissued`.`EnteredBy`,
+	`ident_bookissued`.`Entered`,
+	`ident_bookissued`.`EnteredBy`,
+	`ident_bookissued`.`Remarks`
+FROM `greenbookprime`.`ident_bookissued`;
+
+SET SQL_SAFE_UPDATES=0;
+UPDATE ctadb.tblgreenbookissued a
+INNER JOIN ctadb.lstmadebtype b 
+	ON a.sWhyIssued = b.sMadebDisplayKey
+SET a.nMadebTypeId = b.Id;
