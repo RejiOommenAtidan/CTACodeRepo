62f0bb45ae59c78da1ecafce90756a80ad467a31
Rajen Parekh <rajen.parekh@atidan.com>
2020-10-09 14:54:40 +0530

    Recently searched GB Order done.

--- CTADBL/ViewModels/RecentlySearchedGBVM.cs
+++ CTADBL/ViewModels/RecentlySearchedGBVM.cs
@@ -8,33 +8,33 @@ namespace CTADBL.ViewModels
     public class RecentlySearchedGBVM
     {
         #region Private Properties
-        private RecentlySearchedGB _recentlySearchedGB;
-        private string _sGBID;
+        //private RecentlySearchedGB _recentlySearchedGB = null;
+        private int _nGBID;
         private string? _sPhoto = null;
         #endregion
 
         #region Public Properties
-        public RecentlySearchedGB recentlySearchedGB
-        {
-            get
-            {
-                return _recentlySearchedGB;
-            }
-            set
-            {
-                _recentlySearchedGB = value;
-            }
-        }
+        //public RecentlySearchedGB recentlySearchedGB
+        //{
+        //    get
+        //    {
+        //        return _recentlySearchedGB;
+        //    }
+        //    set
+        //    {
+        //        _recentlySearchedGB = value;
+        //    }
+        //}
 
-        public string sGBID
+        public int nGBID
         {
             get
             {
-                return _sGBID;
+                return _nGBID;
             }
             set
             {
-                _sGBID = value;
+                _nGBID = value;
             }
         }
 
