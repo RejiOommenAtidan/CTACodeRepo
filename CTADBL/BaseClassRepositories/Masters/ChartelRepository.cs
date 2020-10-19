82b4d880af1f5b85a603a6e45645ad8c1fe679a1
Malay Doshi <malay.doshi@atidan.com>
2020-10-19 11:28:39 +0530

    Master Chartel Class Made and Repository

---
+++ CTADBL/BaseClassRepositories/Masters/ChartelRepository.cs
@@ -0,0 +1,91 @@
+﻿using CTADBL.BaseClasses.Masters;
+using CTADBL.QueryBuilder;
+using CTADBL.Repository;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CTADBL.BaseClassRepositories.Masters
+{
+    public class ChartelRepository : ADORepository<Chartel>
+    {
+        #region Constructor
+        public ChartelRepository(string connectionString) : base(connectionString)
+        {
+        }
+        #endregion
+
+        #region Add Call
+        public void Add(Chartel chartel)
+        {
+            var builder = new SqlQueryBuilder<Chartel>(chartel);
+            ExecuteCommand(builder.GetInsertCommand());
+        }
+        #endregion
+
+        #region Update Call
+        public void Update(Chartel chartel)
+        {
+            var builder = new SqlQueryBuilder<Chartel>(chartel);
+            ExecuteCommand(builder.GetUpdateCommand());
+        }
+        #endregion
+
+        #region Delete Call
+        public void Delete(Chartel chartel)
+        {
+            var builder = new SqlQueryBuilder<Chartel>(chartel);
+            ExecuteCommand(builder.GetDeleteCommand());
+        }
+        #endregion
+
+        #region Get Chartel
+        public IEnumerable<Chartel> GetAllChartel()
+        {
+            string sql = @"SELECT `Id`,
+                            `sChartelKey`,
+                            `nChartelValue`,
+                            `dtChartelFrom`,
+                            `dtEntered`,
+                            `nEnteredBy`
+                        FROM `lstchartel`;";
+            using (var command = new MySqlCommand(sql))
+            {
+                return GetRecords(command);
+            }
+        }
+
+        public Chartel GetChartelById(string Id)
+        {
+            string sql = @"SELECT `Id`,
+                            `sChartelKey`,
+                            `nChartelValue`,
+                            `dtChartelFrom`,
+                            `dtEntered`,
+                            `nEnteredBy`
+                        FROM `lstchartel`
+                        WHERE Id = @Id;";
+            using (var command = new MySqlCommand(sql))
+            {
+                command.Parameters.AddWithValue("Id", Id);
+                return GetRecord(command);
+            }
+        }
+        #endregion
+
+        #region Populate Chartel Records
+        public override Chartel PopulateRecord(MySqlDataReader reader)
+        {
+            Chartel chartel = new Chartel();
+            chartel.Id = (int)reader["Id"];
+            chartel.sChartelKey = (string)reader["sChartelKey"];
+            chartel.nChartelValue = (int?)reader["nChartelValue"];
+            chartel.dtChartelFrom = reader.IsDBNull("dtChartelFrom") ? null : (DateTime?)(reader["dtChartelFrom"]);
+            chartel.dtEntered = reader.IsDBNull("dtEntered") ? null : (DateTime?)(reader["dtEntered"]);
+            chartel.nEnteredBy = (int)reader["nEnteredBy"];
+            return chartel;
+        }
+        #endregion
+    }
+}
---
+++ CTADBL/BaseClasses/Masters/Chartel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CTADBL.BaseClasses.Masters
+{
+    [Table("lstchartel")]
+    public class Chartel
+    {
+        #region Private Chartel Properties 
+        private int _Id;
+        private string _sChartelKey;
+        private int? _nChartelValue;
+        private DateTime? _dtChartelFrom;
+        private DateTime? _dtEntered = null;
+        private int _nEnteredBy = 1;
+        #endregion
+
+        #region Public Chartel Properties
+        [Key]
+        public int Id { get { return _Id; } set { _Id = value; } }
+        public string sChartelKey { get { return _sChartelKey; } set { _sChartelKey = value; } }
+        public int? nChartelValue { get { return _nChartelValue; } set { _nChartelValue = value; } }
+        public DateTime? dtChartelFrom { get { return _dtChartelFrom; } set { _dtChartelFrom = value; } }
+        public DateTime? dtEntered { get { return _dtEntered; } set { _dtEntered = value; } }
+        public int nEnteredBy { get { return _nEnteredBy; } set { _nEnteredBy = value; } }
+        #endregion
+    }
+}
