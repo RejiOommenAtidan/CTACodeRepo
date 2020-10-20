9ca428c788ba303cdb9805c3408a301be7ce2bd7
Malay Doshi <malay.doshi@atidan.com>
2020-10-20 14:40:50 +0530

    Audit Log API Calls Added

--- CTADBL/BaseClassRepositories/Transactions/AuditLogRepository.cs
+++ CTADBL/BaseClassRepositories/Transactions/AuditLogRepository.cs
@@ -10,8 +10,8 @@ namespace CTADBL.BaseClassRepositories.Transactions
 {
     public class AuditLogRepository : ADORepository<AuditLog>
     {
-        private static MySqlConnection _connection;
         #region Constructor
+        private static MySqlConnection _connection;
         public AuditLogRepository(string connectionString) : base(connectionString)
         {
             _connection = new MySqlConnection(connectionString);
@@ -38,21 +38,41 @@ namespace CTADBL.BaseClassRepositories.Transactions
             AuditLog auditLog = new AuditLog
             {
                 Id = (int)reader["Id"],
-                sGBID = reader.IsDBNull("sGBID") ? null : (string)reader["sGBID"],
                 dtEntered = reader.IsDBNull("dtEntered") ? null : (DateTime?)(reader["dtEntered"]),
-                nEnteredBy = reader.IsDBNull("nEnteredBy") ? null : (int?)(reader["nEnteredBy"]),
                 nFeatureID = reader.IsDBNull("nFeatureID") ? null : (int?)(reader["nFeatureID"]),
                 nRecordID = reader.IsDBNull("nRecordID") ? null : (int?)(reader["nRecordID"]),
                 nRegionID = reader.IsDBNull("nRegionID") ? null : (int?)(reader["nRegionID"]),
+                sGBID = reader.IsDBNull("sGBID") ? null : (string)reader["sGBID"],
                 sFieldValuesOld = reader.IsDBNull("sFieldValuesOld") ? null : (string)reader["sFieldValuesOld"],
-                sFieldValuesNew = reader.IsDBNull("sFieldValuesNew") ? null : (string)reader["sFieldValuesNew"]
+                sFieldValuesNew = reader.IsDBNull("sFieldValuesNew") ? null : (string)reader["sFieldValuesNew"],
+                nEnteredBy = reader.IsDBNull("nEnteredBy") ? null : (int?)(reader["nEnteredBy"])
             };
-            
             return auditLog;
-
         }
+        #endregion
 
+        #region Add Call
+        public void Add(AuditLog auditLog)
+        {
+            var builder = new SqlQueryBuilder<AuditLog>(auditLog);
+            ExecuteCommand(builder.GetInsertCommand());
+        }
+        #endregion
 
+        #region Update Call
+        public void Update(AuditLog auditLog)
+        {
+            var builder = new SqlQueryBuilder<AuditLog>(auditLog);
+            ExecuteCommand(builder.GetUpdateCommand());
+        }
+        #endregion
+
+        #region Delete Call
+        public void Delete(AuditLog auditLog)
+        {
+            var builder = new SqlQueryBuilder<AuditLog>(auditLog);
+            ExecuteCommand(builder.GetDeleteCommand());
+        }
         #endregion
     }
 }
