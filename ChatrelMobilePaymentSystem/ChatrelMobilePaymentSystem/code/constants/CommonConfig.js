310dff87c913e17d2f11c87b2e71b9dfe876c2e4
Malay Doshi <malay.doshi@atidan.com>
2020-10-29 18:35:54 +0530

    Added Items in ChatrelMObile App
    Removal of Old Web Portlal Project

---
+++ ChatrelMobilePaymentSystem/ChatrelMobilePaymentSystem/code/constants/CommonConfig.js
@@ -0,0 +1,6 @@
+export const sAPIBASEURL = "http://localhost:52013/api";
+export const sAdminEmail = "admin@CTA.com";
+export const sSnackbarAddMessage = "Record Added successfully";
+export const sSnackbarUpdateMessage = "Record Updated successfully";
+export const sDateFormat = "DD-MM-YYYY";
+export const sDateFormatMUIDatepicker = "dd-MM-yyyy";
