2cb858247ad2d245a921ff63404c9b82dc228348
aayushpandya <50677518+aayushpandya@users.noreply.github.com>
2020-10-29 11:44:55 +0530

    WebPayment Template

---
+++ ChatrelWebPaymentSystem/ChatrelWebPaymentSystem/src/views/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+
+//import projectLogo from '../../assets/images/ctalogo.png';
+export default function Home() {
+  return (
+    <>
+      <Card className="card-box mb-spacing-6-x2" style={{ textAlign: 'center', padding: 50 }} >
+       
+        <h1>CTA</h1>
+  
+      </Card>
+    </>
+  );
+}
