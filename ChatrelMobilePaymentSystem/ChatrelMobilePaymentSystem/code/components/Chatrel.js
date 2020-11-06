c0a6a5f097f75859b09231e4a8e28a234b4c2542
Malay Doshi <malay.doshi@atidan.com>
2020-11-07 00:21:10 +0530

    Binding with web API Object

--- ChatrelMobilePaymentSystem/ChatrelMobilePaymentSystem/code/components/Chatrel.js
+++ ChatrelMobilePaymentSystem/ChatrelMobilePaymentSystem/code/components/Chatrel.js
@@ -1,163 +1,138 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Platform, Text, View, ScrollView } from 'react-native';
+import { Switch, Text, View, ScrollView } from 'react-native';
 import { useSelector } from 'react-redux';
 import { Input, Button } from 'react-native-elements';
-import { sDateFormat } from '../constants/CommonConfig';
+import axios from 'axios';
 import Moment from 'moment';
+// import { sDateFormat } from '../constants/CommonConfig';
 
 const oHardcoded = {
-    dtDOB: "01-01-2001",
-    sName: "Malay Doshi",
-    sPaidUntil: "2017",
-    sYearsDue: "3 Years"
+    dtDOB: "01-01-2001"
 };
 
-const aRemainingYearsHardcoded = [
-    {
-        nChatrelYear: 2018,
-        nChatrelMeal: 10,
-        nChatrelBasic: 36,
-        nChatrelLateFeesPercentage: 10,
-        nChatrelLateFees: 4.6,
-        nArrearsAmount: 50.6,
-        nChatrelSalaryAmt: 50,
-        nChatrelDue: 50.6,
-        isEmployed: false
-    },
-    {
-        nChatrelYear: 2019,
-        nChatrelMeal: 10,
-        nChatrelBasic: 36,
-        nChatrelLateFeesPercentage: 10,
-        nChatrelLateFees: 4.6,
-        nArrearsAmount: 50.6,
-        nChatrelSalaryAmt: 50,
-        nChatrelDue: 50.6,
-        isEmployed: false
-    },
-    {
-        nChatrelYear: 2020,
-        nChatrelMeal: 10,
-        nChatrelBasic: 36,
-        nChatrelLateFeesPercentage: 10,
-        nChatrelLateFees: 0,
-        nArrearsAmount: 46,
-        nChatrelDue: 46,
-        nChatrelSalaryAmt: 50,
-        isEmployed: false
-    }
-];
-
 export const Chatrel = props => {
-    const [aRemainingYears, setaRemainingYears] = useState(aRemainingYearsHardcoded);
-    // const [nYearsTotal, setnYearsTotal] = useState("");
+    const sGBIDHardcoded = "7937671";
+    const [sName, setsName] = useState("");
+    const [nPaidUntil, setnPaidUntil] = useState(0);
+    const [sGBID, setsGBID] = useState("");
+    const [nChatrelLateFeesPercentage, setnChatrelLateFeesPercentage] = useState(0);
+    const [nDefaultSalaryAmount, setnDefaultSalaryAmount] = useState(0);
+    const [aGBChatrels, setaGBChatrels] = useState([]);
     const [nGrandTotal, setnGrandTotal] = useState(0);
     const [nAdditionalDonation, setnAdditionalDonation] = useState(0);
     const [nBusinessDonation, setnBusinessDonation] = useState(0);
-    // var nCurrentYear = Moment().year();
-    //console.log(nCurrentYear);
+
     const oCurrentGBDetails = useSelector(state => state.CurrentGBDetailsReducer.oCurrentGBDetails);
-    //console.log(nYearsTotal);
+
     const toggleSwitch = (index, year) => {
-        ////debugger;
-        let payObj = [...aRemainingYears];
-        // let len = aRemainingYears.length;
-        if (year.isEmployed) {
-            // setnTotalForaYear(year.nArrearsAmount);
-            // setnYearsTotal(previousState=>previousState+year.nArrearsAmount);
-            payObj[index].isEmployed = false;
-            // payObj[index].nChatrelDue = payObj[index].nChatrelDue - payObj[index].nChatrelSalaryAmt;
+        let oPayment = [...aGBChatrels];
+        if (year.nChatrelSalaryAmt === 0) {
+            oPayment[index].nChatrelSalaryAmt = nDefaultSalaryAmount;
         }
         else {
-            // setnTotalForaYear(year.nArrearsAmount + year.nChatrelSalaryAmt);
-            // setnYearsTotal(previousState=>previousState+year.nArrearsAmount + year.nChatrelSalaryAmt);
-            payObj[index].isEmployed = true;
-            // payObj[index].nChatrelDue = payObj[index].nChatrelDue + payObj[index].nChatrelSalaryAmt;
-
+            oPayment[index].nChatrelSalaryAmt = 0;
         }
-        setaRemainingYears(payObj);
-        calculate(index);
-        //setIsEnabled(previousState => !previousState);
+        setaGBChatrels(oPayment);
+        calculateMethod(index);
     };
 
-    const calculate = (index) => {
-        let payObj = [...aRemainingYears];
-        let len = aRemainingYears.length;
-        let nSalDecider = payObj[index].isEmployed ? payObj[index].nChatrelSalaryAmt : 0;
+    const calculateMethod = (index) => {
+        let oPayment = [...aGBChatrels];
+        let len = aGBChatrels.length;
         if (index != len - 1) {
-            payObj[index].nChatrelLateFees =
-                (payObj[index].nChatrelBasic + payObj[index].nChatrelMeal + nSalDecider) / payObj[index].nChatrelLateFeesPercentage;
+            oPayment[index].lateFees =
+                (oPayment[index].nChatrelAmount + oPayment[index].nChatrelMeal + oPayment[index].nChatrelSalaryAmt) / nChatrelLateFeesPercentage;
         }
-        payObj[index].nChatrelDue = payObj[index].nChatrelBasic + payObj[index].nChatrelMeal + payObj[index].nChatrelLateFees + nSalDecider;
-        setaRemainingYears(payObj);
-        calcTotal(payObj, nAdditionalDonation, nBusinessDonation);
+        oPayment[index].nChatrelTotalAmount = oPayment[index].nChatrelAmount + oPayment[index].nChatrelMeal + oPayment[index].lateFees + oPayment[index].nChatrelSalaryAmt;
+        setaGBChatrels(oPayment);
+        calcTotal(oPayment, nAdditionalDonation, nBusinessDonation);
 
     };
+
     const calcTotal = (obj, aD, bD) => {
-        //debugger;
         let temptotal = aD + bD;
         obj.forEach((row) => {
-            temptotal += row.nChatrelDue;
+            temptotal += row.nChatrelTotalAmount;
         })
         setnGrandTotal(temptotal);
     };
 
     const handlSubmit = () => {
         let tempSummaryObj = {};
-        let payObj = [...aRemainingYears];
-        let lastindex = payObj.length - 1;
-    
-        tempSummaryObj.nArrearsAmount = nGrandTotal - (payObj[lastindex].nChatrelDue + nBusinessDonation + nAdditionalDonation);
+        let oPayment = [...aGBChatrels];
+        let lastindex = oPayment.length - 1;
+
+        tempSummaryObj.nArrearsAmount = nGrandTotal - (oPayment[lastindex].nChatrelDue + nBusinessDonation + nAdditionalDonation);
         tempSummaryObj.nChatrelTotalAmount = nGrandTotal;
-        tempSummaryObj.nChatrelSalaryAmt = payObj[lastindex].nChatrelSalaryAmt;
+        tempSummaryObj.nChatrelSalaryAmt = oPayment[lastindex].nChatrelSalaryAmt;
         tempSummaryObj.nChatrelBusinessDonationAmt = nBusinessDonation;
         tempSummaryObj.nChatrelAdditionalDonationAmt = nAdditionalDonation;
         tempSummaryObj.sPaidByGBId = "1234567";
-    
+
         let finalObj = {
-          "chatrelPayment": tempSummaryObj,
-          "gbChatrels": aRemainingYears
+            "chatrelPayment": tempSummaryObj,
+            "gbChatrels": aGBChatrels
         }
-    
         console.log("Final Obj:", finalObj)
-      }
+    };
 
     useEffect(() => {
-        calcTotal(aRemainingYearsHardcoded, nAdditionalDonation, nBusinessDonation)
+        calcTotal(aGBChatrels, nAdditionalDonation, nBusinessDonation)
     });
+
+    useEffect(() => {
+        getChatrelDetails();
+    }, []);
+
+    const getChatrelDetails = () => {
+        axios.get(`/ChatrelPayment/DisplayChatrelPayment/sGBID=` + sGBIDHardcoded)
+            .then(resp => {
+                if (resp.status === 200) {
+                    setnChatrelLateFeesPercentage(resp.data.chatrelPayment.nChatrelLateFeesPercentage);
+                    setnDefaultSalaryAmount(resp.data.nDefaultSalaryAmount);
+                    setaGBChatrels(resp.data.gbChatrels);
+                    setsName(resp.data.sName);
+                    setnPaidUntil(resp.data.nPaidUntil);
+                    setsGBID(resp.data.chatrelPayment.sGBId);
+                }
+            })
+            .catch(error => {
+                console.log(error.message);
+                console.log(error.config);
+            });
+    };
 
     return (
         <ScrollView>
             <Text>PERSONAL INFORMATION</Text>
             <Text>Greenbook ID</Text>
-            <Text>{oCurrentGBDetails.sGBID}</Text>
+            <Text>{sGBID}</Text>
             <Text>Date of Birth</Text>
             <Text>{oHardcoded.dtDOB}</Text>
             <Text>Name</Text>
-            <Text>{oHardcoded.sName}</Text>
+            <Text>{sName}</Text>
             <Text>Year of Last Chatrel</Text>
-            <Text>{oHardcoded.sPaidUntil}</Text>
+            <Text>{Moment(nPaidUntil).year()}</Text>
             <Text>Chatrel of Years Due</Text>
-            <Text>{oHardcoded.sYearsDue}</Text>
+            <Text>{Moment().diff(nPaidUntil, 'years')} Years</Text>
             <Text>Chatrel Information</Text>
-            {aRemainingYears.map((year, index) => {
-                //const [isEnabled, setIsEnabled] = useState(year.isEmployed);
-                //const [nTotalForaYear, setnTotalForaYear] = useState(year.nArrearsAmount);
+            {aGBChatrels.map((year, index) => {
                 return (
                     <View key={year.nChatrelYear}>
-                        <Text>Employment Status: {year.isEmployed ? "Employed" : "Not Employed"}</Text>
+                        <Text>Employment Status: {year.nChatrelSalaryAmt === 0 ? "Not Employed" : "Employed"}</Text>
                         <Switch
                             key={year.nChatrelYear}
                             trackColor={{ false: "#767577", true: "#81b0ff" }}
-                            thumbColor={year.isEmployed ? "#f5dd4b" : "#f4f3f4"}
+                            thumbColor={year.nChatrelSalaryAmt === 0 ? "#f4f3f4" : "#f5dd4b"}
                             ios_backgroundColor="#3e3e3e"
                             onValueChange={() => { toggleSwitch(index, year) }}
-                            value={year.isEmployed}
+                            value={year.nChatrelSalaryAmt !== 0}
                         />
                         <Text>{year.nChatrelYear}</Text>
-                        <Text>Basic: {year.nChatrelBasic}</Text>
+                        <Text>Basic: {year.nChatrelAmount}</Text>
                         <Text>Meal: {year.nChatrelMeal}</Text>
-                        <Text>Total: {year.nChatrelDue}</Text>
+                        {year.lateFees !== 0 && <Text>Late Fees: {year.lateFees}</Text>}
+                        <Text>Total: {year.nChatrelTotalAmount}</Text>
                     </View>
                 )
             })}
@@ -171,11 +146,11 @@ export const Chatrel = props => {
                 disableFullscreenUI={true}
                 onChangeText={(value) => {
                     if (value === "") {
-                        calcTotal(aRemainingYears, 0, nBusinessDonation);
+                        calcTotal(aGBChatrels, 0, nBusinessDonation);
                         setnAdditionalDonation(0);
                     }
                     else {
-                        calcTotal(aRemainingYears, parseInt(value), nBusinessDonation);
+                        calcTotal(aGBChatrels, parseInt(value), nBusinessDonation);
                         setnAdditionalDonation(parseInt(value));
                     }
                 }}
@@ -191,11 +166,11 @@ export const Chatrel = props => {
                 disableFullscreenUI={true}
                 onChangeText={(value) => {
                     if (value === "") {
-                        calcTotal(aRemainingYears, nAdditionalDonation, 0);
+                        calcTotal(aGBChatrels, nAdditionalDonation, 0);
                         setnBusinessDonation(0);
                     }
                     else {
-                        calcTotal(aRemainingYears, nAdditionalDonation, parseInt(value));
+                        calcTotal(aGBChatrels, nAdditionalDonation, parseInt(value));
                         setnBusinessDonation(parseInt(value));
                     }
                 }}
