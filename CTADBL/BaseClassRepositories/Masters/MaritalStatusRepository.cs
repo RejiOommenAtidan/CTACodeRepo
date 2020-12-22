d97bb3a31b73d913ce4483aa2aeb86fda8f76084
Malay Doshi <malay.doshi@atidan.com>
2020-12-22 11:22:35 +0530

    Marital Status Addition as base class & Repo

---
+++ CTADBL/BaseClassRepositories/Masters/MaritalStatusRepository.cs
@@ -0,0 +1,58 @@
+﻿using CTADBL.BaseClasses.Masters;
+using CTADBL.QueryBuilder;
+using CTADBL.Repository;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace CTADBL.BaseClassRepositories.Masters
+{
+    public class MaritalStatusRepository : ADORepository<MaritalStatus>
+    {
+        #region Constructor
+        public MaritalStatusRepository(string connectionString) : base(connectionString)
+        {
+        }
+        #endregion
+
+        #region MaritalStatus Add Call
+        public void Add(MaritalStatus maritalStatus)
+        {
+            var builder = new SqlQueryBuilder<MaritalStatus>(maritalStatus);
+            ExecuteCommand(builder.GetInsertCommand());
+        }
+        #endregion
+
+        #region Update MaritalStatus Call
+        public void Update(MaritalStatus maritalStatus)
+        {
+            var builder = new SqlQueryBuilder<MaritalStatus>(maritalStatus);
+            ExecuteCommand(builder.GetUpdateCommand());
+        }
+        #endregion
+
+        #region Delete MaritalStatus Call
+        public void Delete(MaritalStatus maritalStatus)
+        {
+            var builder = new SqlQueryBuilder<MaritalStatus>(maritalStatus);
+            ExecuteCommand(builder.GetDeleteCommand());
+        }
+        #endregion
+
+        #region Populate MaritalStatus Records
+        public override MaritalStatus PopulateRecord(MySqlDataReader reader)
+        {
+            return new MaritalStatus
+            {
+                Id = (int)reader["Id"],
+                sMaritalStatusId = reader.IsDBNull("sMaritalStatusId") ? null : (string)reader["sMaritalStatusId"],
+                sMaritalStatusText = reader.IsDBNull("sMaritalStatusText") ? null : (string)reader["sMaritalStatusText"],
+                dtEntered = reader.IsDBNull("dtEntered") ? null : (DateTime?)(reader["dtEntered"]),
+                nEnteredBy = (int)reader["nEnteredBy"],
+                dtUpdated = reader.IsDBNull("dtUpdated") ? null : (DateTime?)(reader["dtUpdated"]),
+                nUpdatedBy = (int)reader["nUpdatedBy"],
+            };
+        }
+        #endregion
+    }
+}
